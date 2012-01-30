3bd5715091d7915c3d4b55d5ede666cd7ff92c17
Matt Mondok <matt.mondok@gmail.com>
2012-01-30 09:16:43 -0500

    Adding all of zsh

---
+++ zsh/oh-my-zsh/lib/aliases.zsh
@@ -0,0 +1,35 @@
+# Push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
+
+
+# Basic directory operations
+alias ...='cd ../..'
+alias -- -='cd -'
+
+# Super user
+alias _='sudo'
+
+#alias g='grep -in'
+
+# Show history
+alias history='fc -l 1'
+
+# List direcory contents
+alias lsa='ls -a'
+alias l='ls -la'
+alias ll='ls -l'
+alias sl=ls # often screw this up
+
+alias afind='ack-grep -il'
+
+alias subl="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
+
+# Common Directories
+alias www='cd /var/www'
+alias src='cd /usr/local/src'
+alias home='cd ~/'
+alias code='cd ~/Code'
+
+alias memcached='/usr/local/bin/memcached -d'
+alias s='subl'
